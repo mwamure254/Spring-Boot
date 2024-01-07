1f3a9742cb8f6c9a45a3cb1a252cd6793390df8c
mwamu <mwamu@DESKTOP-48R5JPG>
2024-01-07 17:14:58 +0300

    My First SpringBoot Project

---
+++ DakachaPry/src/main/java/com/example/DakachaPry/DakachaPryApplication.java
@@ -0,0 +1,13 @@
+package com.example.DakachaPry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakachaPryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakachaPryApplication.class, args);
+	}
+
+}
---
+++ DakachaPry/src/test/java/com/example/DakachaPry/DakachaPryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DakachaPry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DakachaPryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
