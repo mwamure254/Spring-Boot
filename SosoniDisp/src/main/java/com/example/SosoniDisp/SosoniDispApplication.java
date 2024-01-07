6407d1954178ac3059c3eb08688979919230c65e
mwamu <mwamu@DESKTOP-48R5JPG>
2024-01-07 17:29:49 +0300

    My SpringBoot Project2

---
+++ SosoniDisp/src/main/java/com/example/SosoniDisp/SosoniDispApplication.java
@@ -0,0 +1,13 @@
+package com.example.SosoniDisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosoniDispApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosoniDispApplication.class, args);
+	}
+
+}
---
+++ SosoniDisp/src/test/java/com/example/SosoniDisp/SosoniDispApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SosoniDisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosoniDispApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
