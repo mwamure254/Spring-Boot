7d869101575340c3d41ab60c7cd503bd8a73140b
mwamu <mwamu@Mfano-PC>
2024-10-23 11:42:14 +0300

    mpesa done

---
+++ mfano/src/main/java/com/mfano/mfano/security/ConstantS.java
@@ -0,0 +1,14 @@
+package com.mfano.mfano.security;
+
+public class ConstantS {
+
+    public static final String BASE_URL = "https://sandbox.safaricom.co.ke";
+    public static final String LIPA_NA_MPESA_URL = BASE_URL + "/mpesa/stkpush/v1/processrequest";
+    public static final String TOKEN_URL = BASE_URL + "/oauth/v1/generate?grant_type=client_credentials";
+    
+    public static final String shortcode = "174379"; // Replace with your shortCode
+    public static final String consumerKey = "r6uDTuSiuJqQBzDSMT2UfzRagabKg34oeCXwrxkXjM2kbD7O"; // Replace with your consumer key
+    public static final String consumerSecret = "glZBVbdNJya67a47hqZjSp7c5WMemN5yIWA1pKTYf316qIfxAa2Jz5MKCvyI1Ss8"; // Replace with your consumer secret
+    public static final String passKey="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+    
+}
