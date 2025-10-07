5109b2f9395976eb6059a2369338a38aae45dba6
mwamure254 <mwamuredkwicha@gmail.com>
2025-10-07 14:18:08 +0300

    october new moe

---
+++ moe/src/main/java/com/mfano/moe/MoeApplication.java
@@ -0,0 +1,13 @@
+package com.mfano.moe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoeApplication.class, args);
+	}
+
+}
