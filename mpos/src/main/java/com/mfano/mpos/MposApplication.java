4b59ae63b12657a6ca0635bbe9a9773abc59e6ac
mwamure254 <mwamuredkwicha@gmail.com>
2026-08-16 16:05:23 +0300

    clear

--- mpos/src/main/java/com/mfano/mpos/MposApplication.java
+++ mpos/src/main/java/com/mfano/mpos/MposApplication.java
@@ -2,8 +2,12 @@ package com.mfano.mpos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
 public class MposApplication {
 
 	public static void main(String[] args) {
