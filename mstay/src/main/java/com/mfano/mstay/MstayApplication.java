bb42ea98f18164f46dbb54f5213bef2ab8508bbe
mwamure254 <mwamuredkwicha@gmail.com>
2026-08-16 16:46:58 +0300

    mstay

---
+++ mstay/src/main/java/com/mfano/mstay/MstayApplication.java
@@ -0,0 +1,13 @@
+package com.mfano.mstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstayApplication.class, args);
+	}
+
+}
---
+++ mstay/src/test/java/com/mfano/mstay/MstayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfano.mstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
